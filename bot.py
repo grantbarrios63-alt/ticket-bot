e01ecaf24671885f7c10f4933e8acace04bf432e
grantbarrios63-alt <grant.barrios63@gmail.com>
2026-05-19 21:39:28 -0500

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 
 import os
-TOKEN = os.getenv(MTQ2NTg5NDE1MzQwNDU0NzMzNw.GsDN0T.Z212NmHW2qqU5FEflKHXppIr1du5myVMNuavfw)
+
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
